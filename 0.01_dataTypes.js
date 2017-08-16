1ab4a376f36eac0cc59515e0d250ba2ebd396716
ajdavis16@hotmail.com <ajdavis16@hotmail.com>
2017-08-16 13:12:50 -0400

    Added datatypes

---
+++ 0.01_dataTypes.js
@@ -0,0 +1,71 @@
+// DataTypes
+
+//Strings
+
+"What a Wonderful world";
+
+'Hello you guys';
+'its a wonderful day';
+console.log('Thomas said "i think grass is green!"'.toUpperCase());
+console.log('Couldn\'t things get any better'.length );
+
+
+console.log("Andrew Davis".toLowerCase())
+
+
+             0123456789
+console.log('jay Jackson'[0].toUpperCase())
+
+console.log('jay'[0].toUpperCase() + 'jay'[1] + 'jay'[2]);
+
+console.log('Home'[0].toUpperCase() + 'Home'[1] + 'Home'[2].toUpperCase() + 'Home'[3]);
+
+
+console.log(typeof("Amber"));
+
+//Numbers
+
+console.log(typeof(2));
+
+console.log(2 + 5)  //7
+console.log(2 - 5)
+console.log(2 / 5)
+console.log(2 * 5)
+console.log(10 % 3)
+
+
+
+//Booleans
+
+console.log(true);
+
+/*
+conditional operators
+
+<      less than
+>      greater than
+<=     less than or equal to
+>=     greater than or equal to
+==     equal to
+===    strictly equal to
+!      (Bang) or Not
+!=     not equal to
+
+*/
+
+console.log(4 == 5);
+console.log("test" > "ego");
+console.log(1 == "1");
+console.log(1 === "1");
+
+console.log('/n');
+console.log('/n');
+
+
+
+
+
+
+
+
+
