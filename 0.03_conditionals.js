6388995f0417d6227f4a8cce2b6c3a2e1bd5fb18
ajdavis16@hotmail.com <ajdavis16@hotmail.com>
2017-08-16 15:33:11 -0400

    added conditionals

---
+++ 0.03_conditionals.js
@@ -0,0 +1,62 @@
+// Condidtionals
+/*
+Changing conditional operators
+&&
+||
+
+*/
+
+var name = "kenn";
+if("kenn" == name )  {
+	console.log("Hey your name is " + name);
+} else {
+	console.log("What is your name?");
+};
+
+
+
+
+var userName = 'user';
+
+if(userName[0] != userName[0].toUpperCase()) {
+	console.log('upper case true');
+
+} else {
+	console.log(userName[0].toUpperCase() + userName.substring(1));
+}
+
+
+
+var age = 16
+
+if (age >= 18) {
+	cosole.log("Yay you can vote!");
+} else if (age>= 21) {
+	console.log("Yay you can drink!");
+} else if (age >= 25) {
+	console.log("Yay you get lower rates on your car rental")
+} else {
+	console.log("sorry too young to do anything!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
